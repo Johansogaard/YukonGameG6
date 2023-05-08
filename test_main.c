824c84e1454467695934f29ab95d61a6964b7082
Anders Jefsen <andersjeff397@gmail.com>
2023-05-08 10:11:56 +0200

    shufflelist test

--- test_main.c
+++ test_main.c
@@ -3,36 +3,34 @@
 #include <time.h>
 #include "main.h"
 #include <assert.h>
+bool isDeckEqual(Card* deck1, Card* deck2) {
+    while (deck1 != NULL && deck2 != NULL) {
+        if (deck1->rank != deck2->rank || deck1->suit != deck2->suit) {
+            return false;
+        }
+        deck1 = deck1->nextCardDec;
+        deck2 = deck2->nextCardDec;
+    }
+    return deck1 == NULL && deck2 == NULL;
+}
 
 void test_shuffleList() {
-    // Create a list of cards that is ordered in a specific way
-    Card* head = NULL;
-    for (int i = 0; i < 10; i++) {
-        Card* newCard = malloc(sizeof(Card));
-        newCard->rank = i;
-        newCard->suit = 'H';
-        newCard->nextCardDec = head;
-        head = newCard;
-    }
 
-    // Shuffle the list
-    shuffleList(head);
+    // Create a deck of cards
+    Card* deck = createDeck();
 
-    // Check that the list is shuffled in a different order than the original list
-    int isShuffled = 0;
-    for (int i = 0; i < 10; i++) {
-        if (head->rank != i) {
-            isShuffled = 1;
-            break;
-        }
-        head = head->nextCardDec;
-    }
-    if (!isShuffled) {
-        printf("Error: shuffleList() did not shuffle the list\n");
-    }
+    // Shuffle the deck
+    shuffleList(deck);
+
+    // Check if the deck has been shuffled
+    bool isShuffled = !isDeckEqual(deck, createDeck());
+    printf("Test 01: Shufflelist()\n");
     if (isShuffled) {
-        printf("PASSED: shuffleList() shuffled the list\n");
+        printf("PASSED: shuffleList() shuffled the deck\n");
+    } else {
+        printf("FAILED: shuffleList() did not shuffle the deck\n");
     }
+    printf("\n");
 }
 
 int main() {
